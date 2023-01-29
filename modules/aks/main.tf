0b5d43b71b759460399a012a18926586a073941f
Mathiew Abbas <mathiew95@gmail.com>
2023-01-28 23:05:10 -0600

    Aumb patch 3 (#10)

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -1,2 +1,2 @@
-#asdtest this again masd cos iasxdsass sssssssssssssssssss
-#also again re 1  2 yes as  as ss sadasasda asda kamen ssss
+#asdtest this again masd cos iasxdsass sssss
+#also again re 1  2 yes as  as ss sadasasda asda kamen s
